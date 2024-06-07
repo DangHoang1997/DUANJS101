939ffb47fe10f8cdfb546c11693ef4335ed7f310
DangHoang1997 <s188143@nagoya-ku.ac.jp>
2024-06-07 20:10:12 +0700

    3.commit

---
+++ BASE/index.js
@@ -0,0 +1,55 @@
+console.log('hello');
+
+
+// biến sẽ gọi là variable hoặc data
+// var tenBien = duLieu Camel key
+
+
+// string type 
+var fullName = "Nguyen Van A";
+console.log(fullName);
+// number typpe 
+var age = 18;
+console.log(age);
+// boolean type
+
+var isMale = true;
+var isFemale = false;
+
+// null type
+
+var isMarried =null;
+var is_married = undefined;
+
+
+console.log(isMarried);
+
+var n1 = 2+2;
+
+console.log("n1:", n1);
+console.log("n1:", n1 + n1)
+
+var n2 = 5;
+
+var n3 = n2 +5;
+console.log(n3);
+var n4 = 100 / n2; 
+console.log(n4)
+var n5 = n2 * 2;
+console.log(n5);
+var n6 = 97 % n2;
+console.log(n6);
+
+// khai bao va tao gia tri
+
+var userName = "Alice";
+
+
+// update gia tri bien
+
+userName = "Bob";
+
+console.log(userName);
+
+// code js chay tu tren xuong duoi
+// khi gan gia tri moi cho bien thi gia tri moi se duoc su dung
---
+++ BASE/mainhomework.js
@@ -0,0 +1,18 @@
+// function checkDataType(value){
+//     return typeof value;
+// }
+// console.log(checkDataType(42));
+// console.log(checkDataType('hello'));
+// console.log(checkDataType(true));
+// console.log(checkDataType([1,2,3]));
+// console.log(checkDataType({name: "alice"}));
+
+// function convertToString(value) {
+//     return String(value);
+// }
+
+// // Ví dụ sử dụng
+// console.log(convertToString(42)); // "42"
+// console.log(convertToString(true)); // "true"
+// console.log(convertToString([1, 2, 3])); // "1,2,3"
+// console.log(convertToString({name: "Alice"})); // "[object Object]"
